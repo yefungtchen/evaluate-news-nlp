d0f54b42b848c97510468c376c95e06b566336c0
Ye Fung Tchen <ye.fung.tchen@klqc.de>
2020-05-18 16:16:59 +0200

    Fixing functionality

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,18 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+
+import "./styles/base.scss"
+import "./styles/footer.scss"
+import "./styles/form.scss"
+import "./styles/header.scss"
+import "./styles/resets.scss"
+
+export {
+    checkForName,
+    handleSubmit
+}
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
 console.log("CHANGE!!");
