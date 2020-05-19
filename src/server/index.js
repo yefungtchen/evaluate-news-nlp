437ba96349472da80591f1a5a3f7e99bde3cd1d7
Ye Fung Tchen <ye.fung.tchen@klqc.de>
2020-05-19 08:57:21 +0200

    Adding comments

--- src/server/index.js
+++ src/server/index.js
@@ -2,27 +2,40 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var path = require("path");
-const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 
-var AYLIENTextAPI = require('aylien_textapi');
-var textapi = new AYLIENTextAPI({
+const mockAPIResponse = require("./mockAPI.js");
+
+// Aylien API
+const AYLIENTextAPI = require('aylien_textapi');
+const textapi = new AYLIENTextAPI({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY
 });
 
+// Requires Express to run server and routes
+const express = require("express");
 const app = express();
 
+/** Initialize the main project folder
+ *   This line of code connects our server-side code (the code in the server/index.js file)
+ *   to our client-side code (the browser code written in the files housed in the dist folder).
+ **/
 app.use(express.static("dist"));
-app.use(cors())
+// Configuring that express use cors
+const cors = require("cors");
+app.use(cors());
 
+/** Middleware
+ *   "body-parser extract the entire body portion of an incoming request stream and exposes it on req.body."
+ *   (Source: https://stackoverflow.com/questions/38306569/what-does-body-parser-do-with-express)
+ **/
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 console.log(__dirname);
 
+// Setting up the GET route (Client takes data from Server)
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
 });
@@ -36,4 +49,7 @@ app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
 
-// POST Request: URL in textfield
+// Setting up the POST Route (Client sending data to Server)
+app.post("/sendText", function (req, res) {
+
+});
