4399c4a1bf74a726f1c8bc6757fdc57b1168fe95
Ye Fung Tchen <ye.fung.tchen@klqc.de>
2020-05-20 12:11:48 +0200

    updating comments

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,11 +7,13 @@ function handleSubmit(event) {
 
     // Check the URL which was put into the formfield
     let url = document.getElementById("url").value;
-    // If statement to check if the URL is not valid or valid
+    // If statement to check if ..
     if (!urlCheck(url)) {
+        // .. URL is not valid
         console.log("Your URL is not valid");
         return;
     }
+    // .. URL is valid
     console.log("URL valid: please wait for the response")
     fetch("http://localhost:8080/sendText", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
@@ -20,7 +22,9 @@ function handleSubmit(event) {
         },
         body: JSON.stringify({ url }), // body data type must match "Content-Type" header
     })
+        // Sends a JSON response composed of the specified data
         .then(res => res.json())
+        // Response data from API
         .then(function (response) {
             console.log("Aylien results listed below")
             console.log(response);
